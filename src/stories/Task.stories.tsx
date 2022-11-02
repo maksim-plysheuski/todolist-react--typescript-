419b9b82e3a7024e6a4a6edb315914fdc2545a54
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-11-02 19:10:22 +0300

    storybook for task component

--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -7,35 +7,40 @@ import {action} from "@storybook/addon-actions";
 import TextField from "@mui/material/TextField/TextField";
 import {IconButton} from "@mui/material";
 import {AddBox} from "@mui/icons-material";
+import {Task} from "../Task";
+import {TaskType} from "../Todolist";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'TODOLISTS/AddItemForm',
-  component: AddItemForm,
+  title: 'TODOLISTS/Task',
+  component: Task,
+  args: { //поподут во все истории эти свойства на уровне сторибука
+    changeTaskStatus: action("changeStatus"),
+    changeTaskTitle: action("changeStatus"),
+    removeTask: action("changeStatus"),
+    task: {id: "qqq", isDone: true, title: "sasd"},
+    todolistId: "asdsdfsdf"
+  }
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    addItem: {
-      description: 'Button clicked inside form'
-    },
-  },
-} as ComponentMeta<typeof AddItemForm>;
+} as ComponentMeta<typeof Task>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof AddItemForm> = (args) => <AddItemForm {...args} />;
+const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
 
-const TemplateWithError: ComponentStory<typeof AddItemForm> = (args) => <AddItemForm {...args} />;
-
-export const AddItemFormStory = Template.bind({});
+export const TaskIsDoneStory = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-AddItemFormStory.args = {
-  addItem: action('Button clicked inside form' )
+TaskIsDoneStory.args = {
+
 };
 
-export const TemplateWithError = TemplateWithError.bind({});
+
+export const TaskIsNotStory = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-TemplateWithError.args = {
-  addItem: action('Button clicked inside form' )
+TaskIsNotStory.args = {
+  task: {id: "qqq", isDone: false, title: "HTML"}, //на уровне истории можем переопределить свойства
+
 };
+
 
 
 
